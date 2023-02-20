ae91f5a8c440d6905e0362f725b5f2e87d900e66
matheusnerisxavier <matheus.nerisxa@outlook.com>
2023-02-20 20:50:53 -0300

    controle-series

---
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Serie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SeriesController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        $series = Serie::all();
+        $mensagemSucesso = session('mensagem.sucesso');
+        return view('series.index')->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        $serie = Serie::create($request->all());
+        return to_route('series.index')->with('mensagem.sucesso',"Série '{$serie->nome}' adicionada com sucesso");
+    }
+
+    public function destroy(Serie $series)
+    {
+        $series->delete();
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' removida com sucesso");
+    }
+
+    public function edit(Serie $series)
+    {
+        dd($series->temporadas());
+        return view('series.edit')->with('serie', $series);
+    }
+
+    public function update(Serie $series, SeriesFormRequest $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return to_route('series.index')->with('mensagem.sucesso', "Série '{$series->nome}' atualizada com sucesso!");
+    }
+}
+
---
+++ controle-series/app/Models/Serie.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Serie extends Model
+{
+    use HasFactory;
+    protected $fillable = ['nome'];
+
+    public function temporadas()
+    {
+        return $this->hasMany(Season::class, 'series_id');
+    }
+
+    /**
+     * @return array
+     */
+
+    protected static function booted()
+    {
+        self::addGlobalScope('ordered', function (Builder $queryBuilder){
+           $queryBuilder->orderBy('nome');
+        });
+    }
+}
